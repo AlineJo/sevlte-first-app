6b91971466d468c9d8b2ef391279dda5e2ac311c
AlineJo <josephali1993@gmail.com>
2023-06-20 17:57:37 +0400

    # each example

--- src/routes/logic/+page.svelte
+++ src/routes/logic/+page.svelte
@@ -2,7 +2,7 @@
     import Navbar from "../../lib/components/Navbar.svelte";
     import Paragraph from "../../lib/components/Paragraph.svelte";
     import Button from "../../lib/components/Button.svelte";
-    
+
     let user = { loggedIn: false };
 
     function toggle() {
@@ -10,6 +10,16 @@
     }
 
     let x = 7;
+
+    let cats = [
+        { id: "J---aiyznGQ", name: "Keyboard Cat" },
+        { id: "z_AbfPXTKms", name: "Maru" },
+        { id: "OUtn3pvWmpg", name: "Henri The Existential Cat" },
+    ];
+
+    function removeFirstItem() {
+        cats = cats.slice(1);
+    }
 </script>
 
 <Navbar screen_home="../" />
@@ -22,32 +32,63 @@
         text="Create your package using @sveltejs/package and preview/showcase your
         work with SvelteKit"
     />
- 
-    
-        {#if user.loggedIn}
-            <Button
-                on:click={toggle}
-                cssClasses="btn-danger mt-5"
-                text="Log out"
-            />
-        {:else}
-            <Button
-                on:click={toggle}
-                cssClasses="btn-success mt-5"
-                text="Log in"
-            />
-        {/if}
 
-        <Paragraph cssClasses="mt-5 display-5" text="--------" />
-        {#if x > 10}
-            <p>{x} is greater than 10</p>
-        {:else if 5 > x}
-            <p>{x} is less than 5</p>
-        {:else}
-            <p>{x} is between 5 and 10</p>
-        {/if}
-    
-        
+    {#if user.loggedIn}
+        <Button on:click={toggle} cssClasses="btn-danger mt-5" text="Log out" />
+    {:else}
+        <Button on:click={toggle} cssClasses="btn-success mt-5" text="Log in" />
+    {/if}
+
+    <Paragraph cssClasses="mt-5 display-5" text="--------" />
+    {#if x > 10}
+        <p>{x} is greater than 10</p>
+    {:else if 5 > x}
+        <p>{x} is less than 5</p>
+    {:else}
+        <p>{x} is between 5 and 10</p>
+    {/if}
+
+    <h1>The Famous Cats of YouTube</h1>
+
+    <ul>
+        <!-- open each block -->
+
+        {#each cats as cat (cat.id)}
+            <li id="ytLink">
+                <a
+                    target="_blank"
+                    href="https://www.youtube.com/watch?v={cat.id}"
+                    rel="noreferrer"
+                >
+                    {cat.name}
+                </a>
+            </li>
+        {/each}
+
+        <!-- close each block -->
+    </ul>
+
+    <Paragraph cssClasses="my-5 display-5 text-center" text="--------" />
+
+    <Button
+        on:click={removeFirstItem}
+        cssClasses="btn-danger my-2"
+        text="Remove First Item"
+    />
+
+    <ul>
+        {#each cats as cat, i}
+            <li id="ytLink">
+                <a
+                    target="_blank"
+                    href="https://www.youtube.com/watch?v={cat.id}"
+                    rel="noreferrer"
+                >
+                    {i + 1}. {cat.name}
+                </a>
+            </li>
+        {/each}
+    </ul>
 </div>
 
 <style>
@@ -58,7 +99,10 @@
         background-color: bisque;
     }
 
-    a {
-        padding-bottom: 16px;
+    #ytLink {
+        background-color: wheat;
+        padding: 1rem;
+        margin-bottom: 1rem;
+        border-radius: 1rem;
     }
 </style>
