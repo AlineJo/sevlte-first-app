9c3e78c42f4ca312822477f4a8579aa37aa53e77
AlineJo <josephali1993@gmail.com>
2023-06-20 17:56:54 +0400

    # use each to populate nav bar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,11 +1,22 @@
 <script>
     export let screen_home = "/home";
     let screen_logic = "/logic";
+    let screen_fetch = "/fetch";
+
+    let navs = [
+        { text: "Home", link: screen_home },
+        { text: "logic", link: screen_logic },
+        { text: "fetch", link: screen_fetch },
+    ];
 </script>
 
-<nav>
-    <a class="p-2" href={screen_home}>home</a>
-    <a class="p-2" href={screen_logic}>logic</a>
+<nav class="p-3">
+    {#each navs as n, i (i)}
+        <a
+            class="btn text-uppercase shadow rounded text-primary fs-4 py-2 px-3 me-3"
+            href={n.link}>{n.text}</a
+        >
+    {/each}
 </nav>
 
 <style>
